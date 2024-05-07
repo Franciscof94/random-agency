177216c925e4b08e792767ba3e85b6e5ef90d492
Francisco Ferraro <fran94.ferraro@gmail.com>
2024-05-07 01:59:23 -0300

    fix api email

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -55,7 +55,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     const mailOptionsUser = {
-      from: "Random Agencia",
+      from: '"Random Agencia" <' + process.env.FROM_EMAIL + '>',
       to: email,
       subject: "Nuevo mensaje - RANDOM AGENCIA",
 
