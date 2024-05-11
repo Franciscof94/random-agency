1e2ce7a16860ac7dd70cb4e2800b9dc45deab9a9
Francisco Ferraro <fran94.ferraro@gmail.com>
2024-05-10 23:53:11 -0300

    fix styles responsive

--- src/app/components/AboutUs/Cards.tsx
+++ src/app/components/AboutUs/Cards.tsx
@@ -10,7 +10,7 @@ export const Cards = ({ isMobile }: Props) => {
     return (
         <div>
             <div className='flex'>
-                <div className='w-full max-w-[424px] h-72 sm:h-56 md:h-56 lg:h-64 xl:h-64 border-solid border-[1px] border-black-80 rounded-[70px] sm:rounded-[90px] bg-white'>
+                <div className='w-full max-w-[500px] 2xl:max-w-[670px] h-72 sm:h-56 md:h-56 lg:h-64 xl:h-64 border-solid border-[1px] border-black-80 rounded-[70px] sm:rounded-[90px] bg-white'>
                     <SectionNumbers className='pl-8 pt-4 sm:pl-14 sm:pt-4 md:pl-14 md:pt-4 lg:pl-20 lg:pt-6 xl:pl-20 xl:pt-6 text-xl sm:text-3xl lg:text-4xl'>01</SectionNumbers>
                     <p className=' px-6 text-center sm:text-start sm:p-0 md:px-6 lg:px-20 xl:px-20 sm:py-3 md:py-4 lg:py-5 xl:py-7 text-[13px] sm:text-sm md:text-sm lg:text-base'>Dialogamos y planteamos un reto para tu marca.</p>
                     {!isMobile && <Arrow className='float-end mx-16 py-6' />}
